ee60a22e792bd78d07d1d2d9e55d7421290d0c5c
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-02-26 06:42:11 +0800

    My First Vue.js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HomePage from './components/HomePage.vue';
+
+const Checking : boolean = false;
+
+
+</script>
+
+<template>
+
+<div  v-if="Checking">
+    <HomePage />
+</div>
+
+
+</template>
+
+<style >
+
+</style>
